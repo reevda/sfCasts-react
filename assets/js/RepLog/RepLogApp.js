8eb959ccd19b8ce1bb3b94c3b0a7e8f0f51e60f0
David Reeves <david.reeves@icloud.com>
2020-06-10 19:20:51 +0200

    inital

---
+++ assets/js/RepLog/RepLogApp.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import RepLogs from "./RepLogs";
+import PropTypes from "prop-types";
+
+
+export default class RepLogApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            highlightedRowId: null,
+            repLogs: [
+                { id: 1, reps: 25, itemLabel: 'My laptop', totalWeightLifted: 112.5 },
+                { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
+                { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
+            ]
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(repLogId, event) {
+        this.setState({ highlightedRowId: repLogId })
+    }
+
+    render() {
+        const { highlightedRowId } = this.state;
+        const { withHeart } = this.props;
+
+        return <RepLogs withHeart={ withHeart } highlightedRowId={ highlightedRowId } onRowClick={ this.handleClick }
+                        repLogs={ this.state.repLogs }/>
+
+    }
+}
+
+RepLogApp.propTypes = {
+    withHeart: PropTypes.bool
+}
---
+++ assets/js/rep_log_react.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+import RepLogApp from "./RepLog/RepLogApp";
+
+const shouldShowHeart = true;
+
+render(
+    <RepLogApp withHeart={shouldShowHeart}/>,
+    document.getElementById('lift-stuff-app')
+);
