cee6ad20dc50424e85485d75f856a54b9b2cfacf
David Reeves <david.reeves@icloud.com>
2020-06-13 16:03:17 +0200

    Form Controlled Components

--- assets/js/RepLog/RepLogs.js
+++ assets/js/RepLog/RepLogs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import RepLogList from "./RepLogList";
 import PropTypes from 'prop-types';
 import RepLogCreator from "./RepLogCreator";
+// import RepLogCreator from "./RepLogCreatorControlledComponents";
 
 const calculateTotalWeightFancier = repLogs => repLogs.reduce((total, log) => total + log.totalWeightLifted, 0);
 
